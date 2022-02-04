8cd602a848a5d35fd52c8cb9f1934c264058d107
noka <noka.hieunt@gmail.com>
2022-02-04 15:41:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Kafka with Docker
+
---
+++ src/test/java/com/nokavietnam/springkafkadocker/SpringKafkaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springkafkadocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaDockerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
